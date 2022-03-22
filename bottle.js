183ffbbece4b2a15c86e72d386feebb6a7ccb220
lipi-2 <97337501+lipi-2@users.noreply.github.com>
2022-03-22 15:00:24 +0530

    Add files via upload

--- bottle.js
+++ bottle.js
@@ -1,15 +1,20 @@
+img = "";
 status = "";
+objects = [];
 
 function preload()
 {
     img = loadImage('bottle.jpg');
+    img.width = canvas.width
+    img.height = canvas.height
 }
 
 function setup()
 {
-    canvas = createCanvas(380,380);
+    canvas = createCanvas(500,380);
     canvas.center();
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    objectDetector.detect(img,gotResults);
     document.getElementById("status").innerHTML = "Status : Detecting Objects";
 }
 
@@ -26,4 +31,24 @@ function gotResults(error,results)
         console.error(error);
     }
     console.log(results);
+    objects = results;
+}
+
+function draw()
+{
+    if (status != "")
+    {
+        for (i = 0; i < objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status : Object detected";
+            document.getElementById("number_of_objects").innerHTML = "There are 4 big objects in the image out of which cocossd model has detected " + objects.length;
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x+15, objects[i].y+15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
 }
--- fruit.js
+++ fruit.js
@@ -1,4 +1,6 @@
+img = "";
 status = "";
+objects = [];
 
 function preload()
 {
@@ -7,9 +9,10 @@ function preload()
 
 function setup()
 {
-    canvas = createCanvas(380,380);
+    canvas = createCanvas(500,380);
     canvas.center();
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    objectDetector.detect(img,gotResults);
     document.getElementById("status").innerHTML = "Status : Detecting Objects";
 }
 
@@ -26,4 +29,24 @@ function gotResults(error,results)
         console.error(error);
     }
     console.log(results);
+    objects = results;
+}
+
+function draw()
+{
+    if (status != "")
+    {
+        for (i = 0; i < objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status : Object detected";
+            document.getElementById("number_of_objects").innerHTML = "There are " + objects.length + " big objects in the image out of which cocossd model has detected " + objects.length;
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x+15, objects[i].y+15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
 }
